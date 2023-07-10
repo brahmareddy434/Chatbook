77c31a07cd05458553d66f44fe013843a2e28583
sharanm2 <sharanm933@gmail.com>
2023-07-10 12:20:01 +0530

    db migration and changes in env file

---
+++ schema.py
@@ -0,0 +1,19 @@
+# To avoid confusion between the SQLAlchemy models and the Pydantic models, we will have the file models.py with the SQLAlchemy models, and the file schemas.py with the Pydantic models.
+
+# These Pydantic models define more or less a "schema" (a valid data shape).
+
+# So this will help us avoiding confusion while using both.
+
+
+# create intial pydantic model / schemas
+
+
+from pydantic import BaseModel
+
+class Book(BaseModel):
+    firstname: str
+    lastname: str
+    password: str
+
+    class Config:
+        orm_mode = True
